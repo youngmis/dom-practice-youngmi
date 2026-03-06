c2aed64b0b9337cd8825145e573fb7bb26da8aed
youngmis <fromme478@gmail.com>
2026-03-06 18:16:27 +0900

    dom phase1,2 업로드

--- 02_interactive/1_dom_select/practice/phase1_starter/script.js
+++ 02_interactive/1_dom_select/practice/phase1_starter/script.js
@@ -5,10 +5,15 @@
 // 미션 ① app-title 텍스트 바꾸기
 // 힌트: document.getElementById('app-title')로 선택 후 .textContent 사용
 // 여기에 코드를 작성하세요 ↓
-
+const apptitle = document.getElementById("app-title");
+apptitle.textContent = "나만의 Todo 앱";
 // 미션 ② app-subtitle 텍스트 바꾸기
-
+const appsubtitle = document.getElementById("app-subtitle");
+appsubtitle.textContent = "오늘 영미님의 할 일은 아래와 같아요!";
 // 미션 ③ remaining-count 숫자를 3으로 바꾸기
-
+const count = document.getElementById("remaining-count");
+count.textContent = "3";
 // 미션 ④ todo-list 요소를 콘솔에 출력하기
 // 힌트: console.log()를 사용하세요
+const todolist = document.getElementById("todo-list");
+console.log(todolist);
--- 02_interactive/1_dom_select/practice/phase2_starter/script.js
+++ 02_interactive/1_dom_select/practice/phase2_starter/script.js
@@ -11,6 +11,8 @@ const remainingCount = document.getElementById("remaining-count");
 function updateCount() {
   // 힌트: todoList.children.length 를 활용하세요
   // 여기에 코드를 작성하세요 ↓
+  const count = todoList.children.length;
+  remainingCount.textContent = count;
 }
 
 // 할 일 추가 함수
@@ -24,26 +26,37 @@ function addTodo() {
 
   // ① li 요소 만들기
   // const li = ...
-
+  const li = document.createElement("li");
+  li.className = "todo-item";
   // ② span 요소 만들기 + 텍스트 넣기
   // const span = ...
-
+  const span = document.createElement("span");
+  span.textContent = text;
+  //span.className = "newlist";
   // ③ 삭제 버튼 만들기
   // const deleteBtn = ...
   // deleteBtn.textContent = '삭제'
   // deleteBtn.className = 'btn-delete'
-
+  const deletebtn = document.createElement("button");
+  deletebtn.textContent = "삭제";
+  deletebtn.className = "btn-delete";
   // ④ 삭제 버튼 클릭 시 li 삭제 + 카운트 업데이트
   // deleteBtn.addEventListener('click', function() {
   //     ...
   // });
-
+  deletebtn.addEventListener("click", function () {
+    li.remove();
+    updateCount();
+  });
   // ⑤ li에 span, deleteBtn 붙이기
   // li.appendChild(...)
   // li.appendChild(...)
+  li.appendChild(span);
+  li.appendChild(deletebtn);
 
   // ⑥ todoList에 li 추가하기
   // todoList.appendChild(...)
+  todoList.appendChild(li);
 
   // ⑦ 입력창 비우기
   todoInput.value = "";
