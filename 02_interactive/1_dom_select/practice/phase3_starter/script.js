35f086445bb67f79c7c98e862384ea1112a6b4bc
youngmis <fromme478@gmail.com>
2026-03-06 18:38:26 +0900

    dom phase3 업로드

--- 02_interactive/1_dom_select/practice/phase3_starter/script.js
+++ 02_interactive/1_dom_select/practice/phase3_starter/script.js
@@ -12,6 +12,8 @@ function updateCount() {
   // Phase 2에서 만든 코드를 가져오세요
   // 미션 ③: 완료된 항목(.done)은 제외하고 카운트하기
   // 힌트: todoList.querySelectorAll('.todo-item:not(.done)').length
+  const count = todoList.querySelectorAll(".todo-item:not(.done)").length;
+  remainingCount.textContent = count;
 }
 
 // 할 일 추가 함수 (Phase 2 코드를 가져오세요)
@@ -29,6 +31,8 @@ function addTodo() {
     // e.target.checked 가 true이면 done 클래스 추가
     // false이면 done 클래스 제거
     // 여기에 코드를 작성하세요 ↓
+    li.classList.toggle("done");
+    updateCount();
   });
 
   const span = document.createElement("span");
@@ -57,6 +61,9 @@ addBtn.addEventListener("click", addTodo);
 // 미션 ①: Enter 키 이벤트 추가하기
 todoInput.addEventListener("keydown", function (e) {
   // e.key 값을 콘솔로 확인해보세요
+  if (e.key === "Enter") {
+    addTodo();
+  }
   console.log("눌린 키:", e.key);
 
   // Enter 키일 때 addTodo() 호출하기
